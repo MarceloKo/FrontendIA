165e4123bfed9f23e13108fe5a5208e04468aecc
Marcelo Kohlhase <marceloakohlhase@hotmail.com>
2022-11-04 18:43:13 -0400

    Criado tela de relatório

--- src/pages/Dashboard/Relatorio/index.tsx
+++ src/pages/Dashboard/Relatorio/index.tsx
@@ -1,5 +1,54 @@
-export default function Relatorio(){
+import { BoxRelatorio, ContainerRelatorio } from "./style";
+import { Input, DatePicker } from 'antd'
+import Button from "../../../components/Commons/Button";
+import { Select } from 'antd';
+
+export default function Relatorio() {
+    const { RangePicker } = DatePicker;
+    const onChange = (value: string) => {
+        console.log(`selected ${value}`);
+    };
+
+    const onSearch = (value: string) => {
+        console.log('search:', value);
+    };
+
     return (
-        <h1>relatorio</h1>
+        <ContainerRelatorio>
+            <BoxRelatorio>
+                <h1>Relatório</h1>
+                <form>
+                    <label>Selecione o quarto</label>
+                    <Select
+                        showSearch
+                        placeholder="Select a person"
+                        optionFilterProp="children"
+                        onChange={onChange}
+                        onSearch={onSearch}
+                        filterOption={(input, option) =>
+                            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                        }
+                        options={[
+                            {
+                                value: '1',
+                                label: 'Quarto 1',
+                            },
+                            {
+                                value: '2',
+                                label: 'Quarto 2',
+                            },
+                            {
+                                value: '3',
+                                label: 'Quarto 3',
+                            },
+                        ]}
+                    />
+                    <label>Selecione a data</label>
+                    <RangePicker />
+                    <Button name='Entrar' onClick={() => console.log('Gerar PDF')} width={'150px'} style={{ margin: '10px auto' }} />
+                </form>
+            </BoxRelatorio>
+
+        </ContainerRelatorio>
     )
 }
---
+++ src/pages/Dashboard/Relatorio/style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ContainerRelatorio = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-height: calc(100vh - 115px);
+`
+export const BoxRelatorio = styled.div`
+    background: white;
+    width: 500px;
+    padding: 20px;
+    border-radius: 10px;
+    form{
+        display: flex;
+        flex-direction: column;
+        
+    }
+`
+
+// TOMATO É LARANJA, e tomate é laranja
+
+// TEAM LARANJA
+// GABRIEL
+// MARCELO
+
+//TEAM VERMELHO
+// CRISTHYAN
